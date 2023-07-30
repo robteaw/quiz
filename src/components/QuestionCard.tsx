e77bf7678915d9b25c8fb7a64870cfab02780504
robteaw <robteaw@gmail.com>
2023-07-30 18:20:41 -0500

    3. Minor Fix

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    question: string;
-    answers: string[];
-    callback: any;
-    userAnswer: string; 
-    questionNR: number;
-    totalQuestions: nummber;
-}
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: string;
+  questionNR: number;
+  totalQuestions: number;
+};
 
-const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNR, totalQuestions }) => {
-    <div>
-        <p className="number">Question: {questionNR} / {totalQuestions}</p>
-        <p dangerouslySetInnerHTML={{ --html: question }} />
-    </div>
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNR,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNR} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+  </div>
 );
 
 export default QuestionCard;
