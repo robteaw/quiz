dee4373ffebcc51047bbab9357ce60782b279179
Robert Tea <robteaw@gmail.com>
2023-08-01 23:57:01 -0500

    4. More Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
 // components
 import QuestionCard from './components/QuestionCard';
+// types
+import { Difficulty } from './API';
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
 
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [useAnswers, setUseAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchedQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+
   const startTrivia = async () => {
-      
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
   }
 
   const nextQuestion = () => {
-
   }
 
   return (
@@ -23,8 +35,8 @@ function App() {
     <button className="start" onClick={startTrivia}>Start</button>
     <p className="score">Score</p>
     <p>Loading Questions...</p>
-    <QuestionCard />
-    <button className="next" onclick={nextQuestion}>Next Question</button>
+    {/* <QuestionCard />
+    <button className="next" onclick={nextQuestion}>Next Question</button> */}
     </div>
   );
 }
